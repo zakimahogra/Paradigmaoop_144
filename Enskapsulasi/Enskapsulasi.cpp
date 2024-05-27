9fb180c234e391f8cba8aecdef27d109540789fe
M S I <M S I@MSI>
2024-05-27 10:18:54 +0700

    membuat class remotelampu private dan public

--- Enskapsulasi/Enskapsulasi.cpp
+++ Enskapsulasi/Enskapsulasi.cpp
@@ -13,16 +13,3 @@ public:
 	}
 };
 
-int main() {
-	remoteLampu lampuRumah;
-
-	lampuRumah.setSaklarNo(0, "Lampu Teras Rumah");
-	lampuRumah.setSaklarNo(1, "Lampu Ruang Tamu");
-	lampuRumah.setSaklarNo(2, "Lampu Kamar Tidur");
-	lampuRumah.setSaklarNo(2, "Lampu Dapur");
-
-	cout << lampuRumah.getSaklarNo(0) << endl;
-	cout << lampuRumah.getSaklarNo(1) << endl;
-	cout << lampuRumah.getSaklarNo(2) << endl;
-	cout << lampuRumah.getSaklarNo(3) << endl;
-}
