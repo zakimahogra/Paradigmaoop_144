1da6f4e6a24a3be99309b47616c14f14a3f5a1fd
M S I <M S I@MSI>
2024-05-27 10:35:31 +0700

    membuat class abstarksiklas private dan publis

--- Abstraksi/Abstraksi.cpp
+++ Abstraksi/Abstraksi.cpp
@@ -1,20 +1,18 @@
-// Abstraksi.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
-}
+class AbstraksiKlas {
+private: string x, y;
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+public:
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+	void setXP(string a, string b) {
+		x = a;
+		y = b;
+	}
+	void display() {
+		cout << "x = " << x << endl;
+		cout << "y = " << y << endl;
+	}
+};
+
