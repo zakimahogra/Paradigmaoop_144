c8be4a39ee39393d7ede7541e9762e29e9f82176
M S I <M S I@MSI>
2024-05-27 10:38:05 +0700

    int main abstraksiklass ak

--- Abstraksi/Abstraksi.cpp
+++ Abstraksi/Abstraksi.cpp
@@ -6,7 +6,7 @@ private: string x, y;
 
 public:
 
-	void setXP(string a, string b) {
+	void setXY(string a, string b) {
 		x = a;
 		y = b;
 	}
@@ -16,3 +16,10 @@ public:
 	}
 };
 
+int main() {
+	AbstraksiKlas ak;
+	ak.setXY("Yogyakarta", "Kampus");
+	ak.display();
+
+	return 0;
+}
